c0cf1ab503497c9003d2a8908091e5a03a735a61
trivias_wrestling <totaltrivialelite@gmail.com>
2026-09-04 09:32:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
 console.log("Trivias Wrestling funcionando");
-{
-    name: "Cody Rhodes",
-
-    imagen: "Cody Rhodes trivias.jpg",
-
-    division: "Men's Division",
-
-    victorias: 1000,
-    derrotas: 0204,
-
-    logros: [
-        "2x WWE Undisputed Champion",
-        "2x TNA Champion",
-        "Royal Rumble Winner 2024"
-    ]
-      }
+const luchadores = [
+    {
+        nombre: "Cody Rhodes",
+        imagen: "Cody Rhodes trivias.jpg",
+        division: "Men's Division",
+        victorias: 1000,
+        derrotas: 1000,
+        logros: [
+            "2x WWE Undisputed Champion",
+            "2x TNA Champion",
+            "Royal Rumble Winner 2024"
+        ]
+    }
+];
