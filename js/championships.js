24c6931aa270bc7652b18c1cdb4fdb97e72333cf
trivias_wrestling <totaltrivialelite@gmail.com>
2026-09-04 16:59:13 -0300

    Create championships.js

---
+++ js/championships.js
@@ -0,0 +1,45 @@
+const championships = [
+
+    {
+        title: "WWE Championship",
+        champion: "Cody Rhodes",
+        image: "images/Cody Rhodes trivias.jpg",
+        page: "wwe-championship.html"
+    },
+
+    {
+        title: "AEW World Championship",
+        champion: "VACANT",
+        image: "images/aew-title.jpg",
+        page: "aew-world-championship.html"
+    },
+
+    {
+        title: "TNT Championship",
+        champion: "VACANT",
+        image: "images/tnt-title.jpg",
+        page: "tnt-championship.html"
+    },
+
+    {
+        title: "Women's World Championship",
+        champion: "VACANT",
+        image: "images/womens-title.jpg",
+        page: "womens-world-championship.html"
+    },
+
+    {
+        title: "United States Championship",
+        champion: "VACANT",
+        image: "images/us-title.jpg",
+        page: "united-states-championship.html"
+    },
+
+    {
+        title: "Intercontinental Championship",
+        champion: "VACANT",
+        image: "images/ic-title.jpg",
+        page: "intercontinental-championship.html"
+    }
+
+];
