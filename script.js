ac3d3703859612e344bd175dbde80bc199ba976b
trivias_wrestling <totaltrivialelite@gmail.com>
2026-09-04 09:27:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log("Trivias Wrestling funcionando");
+{
+    name: "Cody Rhodes",
+
+    imagen: "Cody Rhodes trivias.jpg",
+
+    division: "Men's Division",
+
+    victorias: 1000,
+    derrotas: 0204,
+
+    logros: [
+        "2x WWE Undisputed Champion",
+        "2x TNA Champion",
+        "Royal Rumble Winner 2024"
+    ]
+      }
