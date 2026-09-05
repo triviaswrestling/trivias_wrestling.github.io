bd280373438b07330fbdaf573d924ab73e69e13a
trivias_wrestling <totaltrivialelite@gmail.com>
2026-09-05 17:53:05 -0300

    Update wrestlers.js

--- js/wrestlers.js
+++ js/wrestlers.js
@@ -1,24 +1,4 @@
 const wrestlers = [
-
-    {
-        name: "Cody Rhodes",
-        image: "images/Cody Rhodes trivias.jpg",
-        nickname: "The American Nightmare",
-        stable: "",
-        division: "Main Roster",
-        brand: "NXT",
-        overall2026: "0 - 0",
-        careerOverall: "0 - 0",
-        status: "active",
-
-        achievements: [
-            "2x WWE Undisputed Champion",
-            "2x TNA Champion",
-            "Royal Rumble Winner 2024"
-        ]
-    },
-
-
     {
         name: "Bret Hart",
         image: "images/Darkman Hart.jpg",
@@ -34,25 +14,24 @@ const wrestlers = [
             "16x World Champion",
             "5x United States Champion"
         ]
-    },
-
-
+    }, 
     {
-        name: "Shawn Spears",
-        image: "images/shawna spears.jpg",
-        nickname: "",
+        name: "Cody Rhodes",
+        image: "images/Cody Rhodes trivias.jpg",
+        nickname: "The American Nightmare",
         stable: "",
-        division: "NXT",
-        brand: "SmackDown",
-        overall2026: "100 - 0",
+        division: "Main Roster",
+        brand: "RAW",
+        overall2026: "0 - 0",
         careerOverall: "0 - 0",
         status: "active",
 
         achievements: [
-            "16x World Champion",
-            "5x United States Champion"
+            "2x WWE Undisputed Champion",
+            "2x TNA Champion",
+            "Royal Rumble Winner 2024"
         ]
-    },
+    },   
 {
         name: "Jeff Jarrett",
         image: "images/Jeff Jarrett trivias.jpg",
@@ -67,6 +46,36 @@ const wrestlers = [
         achievements: [
             "x5 WWE Undisputed Champion"
         ]
+    },
+    {
+        name: "Seth Rollins",
+        image: "images/Seth Rollins trivias.jpg",
+        nickname: "",
+        stable: "The Vision",
+        division: "Main Roster",
+        brand: "SmackDown",
+        overall2026: "100 - 0",
+        careerOverall: "0 - 0",
+        status: "active",
+
+        achievements: [
+            "16x World Champion",
+            "5x United States Champion"
+        ]
+    },
+    {
+        name: "Shawn Spears",
+        image: "images/shawna spears.jpg",
+        nickname: "",
+        stable: "",
+        division: "NXT",
+        brand: "SmackDown",
+        overall2026: "100 - 0",
+        careerOverall: "0 - 0",
+        status: "active",
+
+        achievements:[ "16x World Champion","5x United States Champion" ]
+        
     },
 ];
 
