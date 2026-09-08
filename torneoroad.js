dda2bd1bb0aca4a3d7bd614a51cbaed4a2186db3
trivias_wrestling <totaltrivialelite@gmail.com>
2026-09-07 22:02:19 -0300

    Update torneoroad.js

--- torneoroad.js
+++ torneoroad.js
@@ -19,48 +19,50 @@ const tournamentData = {
 
     "raw-1": {
 
-    participants: [
-        "Axiom",
-        "Bret Hart",
-        "Jacob Fatu",
-        "Kyle O'Reilly",
-        "Cody Rhodes",
-        "Trick Williams",
-        "Bron Breakker",
-        "Jon Moxley"
-    ],
+        participants: [
+            "Axiom",
+            "Bret Hart",
+            "Jacob Fatu",
+            "Kyle O'Reilly",
+            "Cody Rhodes",
+            "Trick Williams",
+            "Bron Breakker",
+            "Jon Moxley"
+        ],
 
-    matches: [
+        matches: [
 
-        {
-            wrestler1: "Axiom",
-            wrestler2: "Bret Hart",
-            score1: 2,
-            score2: 1
-        },
+            {
+                wrestler1: "Axiom",
+                wrestler2: "Bret Hart",
+                score1: 2,
+                score2: 1
+            },
 
-        {
-            wrestler1: "Jacob Fatu",
-            wrestler2: "Kyle O'Reilly",
-            score1: 2,
-            score2: 3
-        },
+            {
+                wrestler1: "Jacob Fatu",
+                wrestler2: "Kyle O'Reilly",
+                score1: 2,
+                score2: 3
+            },
 
-        {
-            wrestler1: "Cody Rhodes",
-            wrestler2: "Trick Williams",
-            score1: 3,
-            score2: 2
-        },
+            {
+                wrestler1: "Cody Rhodes",
+                wrestler2: "Trick Williams",
+                score1: 3,
+                score2: 2
+            },
 
-        {
-            wrestler1: "Bron Breakker",
-            wrestler2: "Jon Moxley",
-            score1: 1,
-            score2: 3
-        }
+            {
+                wrestler1: "Bron Breakker",
+                wrestler2: "Jon Moxley",
+                score1: 1,
+                score2: 3
+            }
 
-    ]
+        ]
+
+    }
 
 };
 
