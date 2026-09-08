3ee0d52a3579cb8a73351fdd20782ffef1f90b88
trivias_wrestling <totaltrivialelite@gmail.com>
2026-09-08 14:07:43 -0300

    Update tournamentData.js

--- js/tournamentData.js
+++ js/tournamentData.js
@@ -13,6 +13,7 @@ const tournamentData = {
 
     /* =========================================
        RAW 1
+       CODY = WWE CHAMPION
        ========================================= */
 
     "raw-1": {
@@ -30,48 +31,175 @@ const tournamentData = {
 
         ],
 
+        images: {
+
+            "Cody Rhodes":
+                "images/Cody WWE Champion.jpg"
+
+        },
 
         matches: [
 
             {
                 date: 1,
-
                 wrestler1: "Axiom",
                 wrestler2: "Bret Hart",
-
                 score1: 2,
                 score2: 1
             },
 
-
             {
                 date: 1,
-
                 wrestler1: "Jacob Fatu",
                 wrestler2: "Kyle O'Reilly",
-
                 score1: 2,
                 score2: 3
             },
 
-
             {
                 date: 1,
-
                 wrestler1: "Cody Rhodes",
                 wrestler2: "Trick Williams",
-
                 score1: 3,
                 score2: 2
             },
 
+            {
+                date: 1,
+                wrestler1: "Bron Breakker",
+                wrestler2: "Jon Moxley",
+                score1: 1,
+                score2: 1
+            }
+
+        ]
+
+    },
+
+
+    /* =========================================
+       RAW 2
+       CODY = ACTUAL
+       ========================================= */
+
+    "raw-2": {
+
+        participants: [
+
+            "Axiom",
+            "Bret Hart",
+            "Jacob Fatu",
+            "Kyle O'Reilly",
+            "Cody Rhodes",
+            "Trick Williams",
+            "Bron Breakker",
+            "Jon Moxley"
+
+        ],
+
+        images: {
+
+            "Cody Rhodes":
+                "images/Cody Actual.jpg"
+
+        },
+
+        matches: [
 
             {
                 date: 1,
+                wrestler1: "Cody Rhodes",
+                wrestler2: "Axiom",
+                score1: 2,
+                score2: 1
+            },
 
+            {
+                date: 1,
+                wrestler1: "Bret Hart",
+                wrestler2: "Jacob Fatu",
+                score1: 1,
+                score2: 2
+            },
+
+            {
+                date: 1,
+                wrestler1: "Kyle O'Reilly",
+                wrestler2: "Trick Williams",
+                score1: 2,
+                score2: 2
+            },
+
+            {
+                date: 1,
                 wrestler1: "Bron Breakker",
                 wrestler2: "Jon Moxley",
+                score1: 3,
+                score2: 1
+            }
 
+        ]
+
+    },
+
+
+    /* =========================================
+       RAW 3
+       CODY = USA CHAMPION
+       ========================================= */
+
+    "raw-3": {
+
+        participants: [
+
+            "Axiom",
+            "Bret Hart",
+            "Jacob Fatu",
+            "Kyle O'Reilly",
+            "Cody Rhodes",
+            "Trick Williams",
+            "Bron Breakker",
+            "Jon Moxley"
+
+        ],
+
+        images: {
+
+            "Cody Rhodes":
+                "images/Cody USA Champion.jpg"
+
+        },
+
+        matches: [
+
+            {
+                date: 1,
+                wrestler1: "Cody Rhodes",
+                wrestler2: "Bret Hart",
+                score1: 3,
+                score2: 0
+            },
+
+            {
+                date: 1,
+                wrestler1: "Axiom",
+                wrestler2: "Jacob Fatu",
+                score1: 1,
+                score2: 2
+            },
+
+            {
+                date: 1,
+                wrestler1: "Kyle O'Reilly",
+                wrestler2: "Bron Breakker",
+                score1: 2,
+                score2: 1
+            },
+
+            {
+                date: 1,
+                wrestler1: "Trick Williams",
+                wrestler2: "Jon Moxley",
                 score1: 1,
                 score2: 1
             }
