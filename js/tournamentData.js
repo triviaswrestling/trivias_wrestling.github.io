e7b15f62f26e11f9d83ee59abea85151bf68acad
trivias_wrestling <totaltrivialelite@gmail.com>
2026-09-08 15:03:06 -0300

    Update tournamentData.js

--- js/tournamentData.js
+++ js/tournamentData.js
@@ -78,5 +78,56 @@ const tournamentData = {
         {date:7,wrestler1:"Kyle O'Reilly",wrestler2:"Bron Breakker",score1:1,score2:3},
         {date:7,wrestler1:"Cody Rhodes",wrestler2:"Trick Williams",score1:2,score2:0}
     ]
-}
+},
+
+                  "smackdown-4":{
+    startDate:"08/09/2026",
+    participants:["Charlie Dempsey","Christian Cage","Finn Balor","Kyle O'Reilly","LA Knight","Randy Orton","Seth Rollins","Swerve Strickland"],
+    images:{
+        "Charlie Dempsey":"images/Vacante.jpg",
+        "Christian Cage":"images/Vacante.jpg",
+        "Finn Balor":"images/Vacante.jpg",
+        "Kyle O'Reilly":"images/Vacante.jpg",
+        "LA Knight":"images/Vacante.jpg",
+        "Randy Orton":"images/Vacante.jpg",
+        "Seth Rollins":"images/Seth Rollins trivias.jpg",
+        "Swerve Strickland":"images/Vacante.jpg"
+    },
+    matches:[
+        {date:1,wrestler1:"Charlie Dempsey",wrestler2:"Christian Cage",score1:2,score2:1},
+        {date:1,wrestler1:"Finn Balor",wrestler2:"Kyle O'Reilly",score1:2,score2:3},
+        {date:1,wrestler1:"LA Knight",wrestler2:"Randy Orton",score1:3,score2:2},
+        {date:1,wrestler1:"Seth Rollins",wrestler2:"Swerve Strickland",score1:1,score2:1},
+
+        {date:2,wrestler1:"Charlie Dempsey",wrestler2:"Finn Balor",score1:2,score2:1},
+        {date:2,wrestler1:"Christian Cage",wrestler2:"LA Knight",score1:1,score2:2},
+        {date:2,wrestler1:"Kyle O'Reilly",wrestler2:"Seth Rollins",score1:2,score2:2},
+        {date:2,wrestler1:"Randy Orton",wrestler2:"Swerve Strickland",score1:3,score2:1},
+
+        {date:3,wrestler1:"Charlie Dempsey",wrestler2:"LA Knight",score1:1,score2:2},
+        {date:3,wrestler1:"Finn Balor",wrestler2:"Seth Rollins",score1:2,score2:3},
+        {date:3,wrestler1:"Christian Cage",wrestler2:"Randy Orton",score1:1,score2:1},
+        {date:3,wrestler1:"Kyle O'Reilly",wrestler2:"Swerve Strickland",score1:2,score2:0},
+
+        {date:4,wrestler1:"Charlie Dempsey",wrestler2:"Seth Rollins",score1:1,score2:3},
+        {date:4,wrestler1:"LA Knight",wrestler2:"Kyle O'Reilly",score1:2,score2:1},
+        {date:4,wrestler1:"Finn Balor",wrestler2:"Randy Orton",score1:0,score2:2},
+        {date:4,wrestler1:"Christian Cage",wrestler2:"Swerve Strickland",score1:2,score2:2},
+
+        {date:5,wrestler1:"Charlie Dempsey",wrestler2:"Kyle O'Reilly",score1:2,score2:1},
+        {date:5,wrestler1:"Seth Rollins",wrestler2:"Randy Orton",score1:3,score2:2},
+        {date:5,wrestler1:"LA Knight",wrestler2:"Swerve Strickland",score1:1,score2:2},
+        {date:5,wrestler1:"Finn Balor",wrestler2:"Christian Cage",score1:2,score2:0},
+
+        {date:6,wrestler1:"Charlie Dempsey",wrestler2:"Randy Orton",score1:1,score2:3},
+        {date:6,wrestler1:"Kyle O'Reilly",wrestler2:"Christian Cage",score1:2,score2:1},
+        {date:6,wrestler1:"Seth Rollins",wrestler2:"Swerve Strickland",score1:2,score2:2},
+        {date:6,wrestler1:"LA Knight",wrestler2:"Finn Balor",score1:3,score2:1},
+
+        {date:7,wrestler1:"Charlie Dempsey",wrestler2:"Swerve Strickland",score1:2,score2:1},
+        {date:7,wrestler1:"Randy Orton",wrestler2:"Kyle O'Reilly",score1:2,score2:2},
+        {date:7,wrestler1:"Seth Rollins",wrestler2:"Christian Cage",score1:3,score2:1},
+        {date:7,wrestler1:"LA Knight",wrestler2:"Finn Balor",score1:1,score2:2}
+    ]
+},
 };
