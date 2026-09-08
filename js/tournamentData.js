388131250c0b41afae064b3c4e67e8ef748720cb
trivias_wrestling <totaltrivialelite@gmail.com>
2026-09-08 14:12:09 -0300

    Update tournamentData.js

--- js/tournamentData.js
+++ js/tournamentData.js
@@ -3,209 +3,80 @@
    TOURNAMENT DATA
    ========================================= */
 
-
-/* =========================================
-   TOURNAMENT MATCH DATA
-   ========================================= */
-
 const tournamentData = {
 
-
-    /* =========================================
-       RAW 1
-       CODY = WWE CHAMPION
-       ========================================= */
-
     "raw-1": {
-
-        participants: [
-
-            "Axiom",
-            "Bret Hart",
-            "Jacob Fatu",
-            "Kyle O'Reilly",
-            "Cody Rhodes",
-            "Trick Williams",
-            "Bron Breakker",
-            "Jon Moxley"
-
-        ],
-
+        participants: ["Axiom","Bret Hart","Jacob Fatu","Kyle O'Reilly","Cody Rhodes","Trick Williams","Bron Breakker","Jon Moxley"],
         images: {
-
-            "Cody Rhodes":
-                "images/Cody WWE Champion.jpg"
-
+            "Cody Rhodes": "images/Cody WWE Champion.jpg"
         },
-
         matches: [
-
-            {
-                date: 1,
-                wrestler1: "Axiom",
-                wrestler2: "Bret Hart",
-                score1: 2,
-                score2: 1
-            },
-
-            {
-                date: 1,
-                wrestler1: "Jacob Fatu",
-                wrestler2: "Kyle O'Reilly",
-                score1: 2,
-                score2: 3
-            },
-
-            {
-                date: 1,
-                wrestler1: "Cody Rhodes",
-                wrestler2: "Trick Williams",
-                score1: 3,
-                score2: 2
-            },
-
-            {
-                date: 1,
-                wrestler1: "Bron Breakker",
-                wrestler2: "Jon Moxley",
-                score1: 1,
-                score2: 1
-            }
-
+            {date:1,wrestler1:"Axiom",wrestler2:"Bret Hart",score1:2,score2:1},
+            {date:1,wrestler1:"Jacob Fatu",wrestler2:"Kyle O'Reilly",score1:2,score2:3},
+            {date:1,wrestler1:"Cody Rhodes",wrestler2:"Trick Williams",score1:3,score2:2},
+            {date:1,wrestler1:"Bron Breakker",wrestler2:"Jon Moxley",score1:1,score2:1}
         ]
-
     },
-
-
-    /* =========================================
-       RAW 2
-       CODY = ACTUAL
-       ========================================= */
 
     "raw-2": {
-
-        participants: [
-
-            "Axiom",
-            "Bret Hart",
-            "Jacob Fatu",
-            "Kyle O'Reilly",
-            "Cody Rhodes",
-            "Trick Williams",
-            "Bron Breakker",
-            "Jon Moxley"
-
-        ],
-
+        participants: ["Axiom","Bret Hart","Jacob Fatu","Kyle O'Reilly","Cody Rhodes","Trick Williams","Bron Breakker","Jon Moxley"],
         images: {
-
-            "Cody Rhodes":
-                "images/Cody Actual.jpg"
-
+            "Cody Rhodes": "images/Cody Actual.jpg"
         },
-
         matches: [
-
-            {
-                date: 1,
-                wrestler1: "Cody Rhodes",
-                wrestler2: "Axiom",
-                score1: 2,
-                score2: 1
-            },
-
-            {
-                date: 1,
-                wrestler1: "Bret Hart",
-                wrestler2: "Jacob Fatu",
-                score1: 1,
-                score2: 2
-            },
-
-            {
-                date: 1,
-                wrestler1: "Kyle O'Reilly",
-                wrestler2: "Trick Williams",
-                score1: 2,
-                score2: 2
-            },
-
-            {
-                date: 1,
-                wrestler1: "Bron Breakker",
-                wrestler2: "Jon Moxley",
-                score1: 3,
-                score2: 1
-            }
-
+            {date:1,wrestler1:"Cody Rhodes",wrestler2:"Axiom",score1:2,score2:1},
+            {date:1,wrestler1:"Bret Hart",wrestler2:"Jacob Fatu",score1:1,score2:2},
+            {date:1,wrestler1:"Kyle O'Reilly",wrestler2:"Trick Williams",score1:2,score2:2},
+            {date:1,wrestler1:"Bron Breakker",wrestler2:"Jon Moxley",score1:3,score2:1}
         ]
-
     },
 
-
-    /* =========================================
-       RAW 3
-       CODY = USA CHAMPION
-       ========================================= */
-
     "raw-3": {
-
-        participants: [
-
-            "Axiom",
-            "Bret Hart",
-            "Jacob Fatu",
-            "Kyle O'Reilly",
-            "Cody Rhodes",
-            "Trick Williams",
-            "Bron Breakker",
-            "Jon Moxley"
-
-        ],
-
+        participants: ["Axiom","Bret Hart","Jacob Fatu","Kyle O'Reilly","Cody Rhodes","Trick Williams","Bron Breakker","Jon Moxley"],
         images: {
-
-            "Cody Rhodes":
-                "images/Cody USA Champion.jpg"
-
+            "Cody Rhodes": "images/Cody USA Champion.jpg"
         },
-
         matches: [
-
-            {
-                date: 1,
-                wrestler1: "Cody Rhodes",
-                wrestler2: "Bret Hart",
-                score1: 3,
-                score2: 0
-            },
-
-            {
-                date: 1,
-                wrestler1: "Axiom",
-                wrestler2: "Jacob Fatu",
-                score1: 1,
-                score2: 2
-            },
-
-            {
-                date: 1,
-                wrestler1: "Kyle O'Reilly",
-                wrestler2: "Bron Breakker",
-                score1: 2,
-                score2: 1
-            },
-
-            {
-                date: 1,
-                wrestler1: "Trick Williams",
-                wrestler2: "Jon Moxley",
-                score1: 1,
-                score2: 1
-            }
-
+            {date:1,wrestler1:"Cody Rhodes",wrestler2:"Bret Hart",score1:3,score2:0},
+            {date:1,wrestler1:"Axiom",wrestler2:"Jacob Fatu",score1:1,score2:2},
+            {date:1,wrestler1:"Kyle O'Reilly",wrestler2:"Bron Breakker",score1:2,score2:1},
+            {date:1,wrestler1:"Trick Williams",wrestler2:"Jon Moxley",score1:1,score2:1}
         ]
+    },
 
+    "raw-4": {
+        participants: ["Axiom","Bret Hart","Jacob Fatu","Kyle O'Reilly","Cody Rhodes","Trick Williams","Bron Breakker","Jon Moxley"],
+        images: {"Axiom":"images/Vacante.jpg","Bret Hart":"images/Vacante.jpg","Jacob Fatu":"images/Vacante.jpg","Kyle O'Reilly":"images/Vacante.jpg","Cody Rhodes":"images/Vacante.jpg","Trick Williams":"images/Vacante.jpg","Bron Breakker":"images/Vacante.jpg","Jon Moxley":"images/Vacante.jpg"},
+        matches: [
+            {date:1,wrestler1:"Axiom",wrestler2:"Bret Hart",score1:2,score2:1},
+            {date:1,wrestler1:"Jacob Fatu",wrestler2:"Kyle O'Reilly",score1:3,score2:2},
+            {date:1,wrestler1:"Cody Rhodes",wrestler2:"Trick Williams",score1:1,score2:1},
+            {date:1,wrestler1:"Bron Breakker",wrestler2:"Jon Moxley",score1:2,score2:0},
+            {date:2,wrestler1:"Axiom",wrestler2:"Jacob Fatu",score1:1,score2:3},
+            {date:2,wrestler1:"Bret Hart",wrestler2:"Cody Rhodes",score1:2,score2:0},
+            {date:2,wrestler1:"Kyle O'Reilly",wrestler2:"Bron Breakker",score1:1,score2:1},
+            {date:2,wrestler1:"Trick Williams",wrestler2:"Jon Moxley",score1:3,score2:2},
+            {date:3,wrestler1:"Axiom",wrestler2:"Cody Rhodes",score1:2,score2:3},
+            {date:3,wrestler1:"Jacob Fatu",wrestler2:"Bron Breakker",score1:1,score2:0},
+            {date:3,wrestler1:"Bret Hart",wrestler2:"Trick Williams",score1:2,score2:2},
+            {date:3,wrestler1:"Kyle O'Reilly",wrestler2:"Jon Moxley",score1:0,score2:2},
+            {date:4,wrestler1:"Axiom",wrestler2:"Bron Breakker",score1:3,score2:1},
+            {date:4,wrestler1:"Cody Rhodes",wrestler2:"Jacob Fatu",score1:2,score2:2},
+            {date:4,wrestler1:"Bret Hart",wrestler2:"Kyle O'Reilly",score1:1,score2:3},
+            {date:4,wrestler1:"Trick Williams",wrestler2:"Jon Moxley",score1:1,score2:2},
+            {date:5,wrestler1:"Axiom",wrestler2:"Kyle O'Reilly",score1:2,score2:1},
+            {date:5,wrestler1:"Bron Breakker",wrestler2:"Cody Rhodes",score1:0,score2:2},
+            {date:5,wrestler1:"Jacob Fatu",wrestler2:"Trick Williams",score1:3,score2:1},
+            {date:5,wrestler1:"Bret Hart",wrestler2:"Jon Moxley",score1:1,score2:1},
+            {date:6,wrestler1:"Axiom",wrestler2:"Trick Williams",score1:0,score2:2},
+            {date:6,wrestler1:"Kyle O'Reilly",wrestler2:"Cody Rhodes",score1:1,score2:3},
+            {date:6,wrestler1:"Jacob Fatu",wrestler2:"Jon Moxley",score1:2,score2:1},
+            {date:6,wrestler1:"Bret Hart",wrestler2:"Bron Breakker",score1:0,score2:2},
+            {date:7,wrestler1:"Axiom",wrestler2:"Jon Moxley",score1:2,score2:2},
+            {date:7,wrestler1:"Bret Hart",wrestler2:"Jacob Fatu",score1:1,score2:3},
+            {date:7,wrestler1:"Kyle O'Reilly",wrestler2:"Trick Williams",score1:2,score2:0},
+            {date:7,wrestler1:"Cody Rhodes",wrestler2:"Bron Breakker",score1:3,score2:1}
+        ]
     }
 
 };
