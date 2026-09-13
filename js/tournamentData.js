ac2ce5be01cf4039b2579f78961f788f2ac1dd10
trivias_wrestling <totaltrivialelite@gmail.com>
2026-09-13 00:02:13 -0300

    Update tournamentData.js

--- js/tournamentData.js
+++ js/tournamentData.js
@@ -135,7 +135,108 @@ const tournamentData = {
         matches:[]
     }
 
-};
+},
+    "campeonato-2":{
+    zones:{
+        RAW:[
+            "Roman Reigns",
+            "AJ Styles",
+            "Kofi Kingston",
+            "Damian Priest",
+            "Rob Van Dam"
+        ],
+        SMACKDOWN:[
+            "Cody Rhodes",
+            "John Cena",
+            "RICOCHET",
+            "Adam Cole",
+            "The Rock"
+        ]
+    },
+
+    champion:"Bryan Danielson",
+
+    phases:[
+        "RAW 1",
+        "RAW 2",
+        "RAW 3",
+        "RAW 4",
+        "RAW 5",
+        "SMACKDOWN 1",
+        "SMACKDOWN 2",
+        "SMACKDOWN 3",
+        "SMACKDOWN 4",
+        "SMACKDOWN 5",
+        "WRESTLEMANIA"
+    ],
+
+    format:"TWO_ZONES_ELIMINATION",
+
+    events:{
+        raw:{
+            "RAW 1":"raw-1",
+            "RAW 2":"raw-2",
+            "RAW 3":"raw-3",
+            "RAW 4":"raw-4",
+            "RAW 5":"raw-5"
+        },
+
+        smackdown:{
+            "SMACKDOWN 1":"smackdown-1",
+            "SMACKDOWN 2":"smackdown-2",
+            "SMACKDOWN 3":"smackdown-3",
+            "SMACKDOWN 4":"smackdown-4",
+            "SMACKDOWN 5":"smackdown-5"
+        },
+
+        final:"wrestlemania-i"
+    },
+
+    matches:{
+        "raw-1":{
+            date:"22/03/2023",
+            results:[
+                {type:"SINGLES",position:"OPENER",wrestler1:"AJ Styles",wrestler2:"Kofi Kingston",score1:3,score2:1},
+                {type:"SINGLES",wrestler1:"Damian Priest",wrestler2:"Rob Van Dam",score1:5,score2:0},
+                {type:"SINGLES",position:"MAIN EVENT",wrestler1:"Roman Reigns",wrestler2:"Cody Rhodes",score1:2,score2:1}
+            ]
+        },
+
+        "raw-2":{
+            date:"24/03/2023",
+            results:[
+                {type:"SINGLES",position:"OPENER",wrestler1:"Roman Reigns",wrestler2:"AJ Styles",score1:2,score2:2},
+                {type:"SINGLES",position:"MAIN EVENT",wrestler1:"Kofi Kingston",wrestler2:"Rob Van Dam",score1:5,score2:0}
+            ]
+        },
+
+        "raw-3":{
+            date:"31/03/2023",
+            results:[
+                {type:"SINGLES",position:"OPENER",wrestler1:"Rob Van Dam",wrestler2:"Roman Reigns",score1:0,score2:5},
+                {type:"SINGLES",wrestler1:"Damian Priest",wrestler2:"Kofi Kingston",score1:1,score2:2},
+                {type:"SINGLES",position:"MAIN EVENT",wrestler1:"AJ Styles",wrestler2:"John Cena",score1:0,score2:3}
+            ]
+        },
+
+        "raw-4":{
+            date:"03/04/2023",
+            results:[
+                {type:"SINGLES",position:"OPENER",wrestler1:"Roman Reigns",wrestler2:"Damian Priest",score1:2,score2:0},
+                {type:"SINGLES",position:"MAIN EVENT",wrestler1:"AJ Styles",wrestler2:"Rob Van Dam",score1:5,score2:0}
+            ]
+        },
+
+        "raw-5":{
+            date:"05/04/2023",
+            results:[
+                {type:"SINGLES",position:"OPENER",wrestler1:"Roman Reigns",wrestler2:"Kofi Kingston",score1:4,score2:0},
+                {type:"SINGLES",wrestler1:"Damian Priest",wrestler2:"AJ Styles",score1:0,score2:2},
+                {type:"SINGLES",position:"MAIN EVENT",wrestler1:"The Rock",wrestler2:"Rob Van Dam",score1:0,score2:5}
+            ]
+        }
+    }
+},
 "campeonato-1":{
     participants:[
         "Cody Rhodes",
@@ -181,5 +282,5 @@ const tournamentData = {
     },
 
     matches:[]
-},
+        };
 alert("TOURNAMENT DATA CARGADO");
