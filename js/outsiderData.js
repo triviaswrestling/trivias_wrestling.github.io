eebfe16157a91439ae2b74e34bde53ae14ea86af
trivias_wrestling <totaltrivialelite@gmail.com>
2026-09-13 19:01:16 -0300

    Create outsiderData.js

---
+++ js/outsiderData.js
@@ -0,0 +1,66 @@
+/* =========================================
+   MI WRESTLING
+   OUTSIDER TOURNAMENT DATA
+   SPEED / AEW / TNA / AAA
+   ========================================= */
+
+const outsiderData = {
+
+    /* =========================================
+       SPEED
+       ========================================= */
+
+    "outsider-speed-1":{
+
+        brand:"SPEED",
+        promotion:"WWE",
+        format:"BRACKET",
+
+        roster:[
+            "Bron Breakker",
+            "Aleister Black",
+            "Tommaso Ciampa",
+            "CM Punk",
+            "Jey Uso",
+            "Edge",
+            "Rob Van Dam",
+            "Wade Barrett"
+        ],
+
+        shows:{
+
+            "speed-1":{
+                date:"08/08/2024",
+                matches:[
+                    ["Bron Breakker","Aleister Black",1,2],
+                    ["Tommaso Ciampa","CM Punk",2,1]
+                ]
+            },
+
+            "speed-2":{
+                date:"15/08/2024",
+                matches:[
+                    ["Jey Uso","Edge",1,2],
+                    ["Rob Van Dam","Wade Barrett",0,3]
+                ]
+            },
+
+            "speed-3":{
+                date:"22/08/2024",
+                matches:[
+                    ["Aleister Black","Edge",0,3],
+                    ["Tommaso Ciampa","Wade Barrett",3,0]
+                ]
+            }
+
+        },
+
+        finalEvent:"summerslam-2024",
+
+        finalMatches:[
+            ["Edge","Tommaso Ciampa",3,2]
+        ]
+
+    }
+
+};
