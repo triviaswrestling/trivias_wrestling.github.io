be34d1a86a249c819484370438ce79f28d832edf
trivias_wrestling <totaltrivialelite@gmail.com>
2026-09-13 19:24:04 -0300

    Update outsiderData.js

--- js/outsiderData.js
+++ js/outsiderData.js
@@ -5,7 +5,21 @@
    ========================================= */
 
 const outsiderData = {
-
+// SPEED #2
+"outsider-speed-2":{
+    brand:"SPEED",
+    promotion:"WWE",
+    format:"BRACKET",
+    roster:["Ilja Dragunov","Kyle O'Reilly","AJ Styles","Johnny Gargano","Rey Mysterio","LA Knight","Sheamus","Chad Gable"],
+    shows:{
+        "speed-4":{date:"29/08/2024",matches:[["Ilja Dragunov","Kyle O'Reilly",0,3],["AJ Styles","Johnny Gargano",3,0]]},
+        "speed-5":{date:"05/09/2024",matches:[["Rey Mysterio","LA Knight",2,1],["Sheamus","Chad Gable",0,3]]},
+        "speed-6":{date:"12/09/2024",matches:[["Kyle O'Reilly","Rey Mysterio",0,3],["AJ Styles","Chad Gable",3,0]]},
+        "speed-7":{date:"19/09/2024",matches:[["Rey Mysterio","AJ Styles",1,2]]}
+    },
+    finalEvent:"bash-in-berlin-2024",
+    finalMatches:[["Edge","AJ Styles",5,0]]},
+  
     /* =========================================
        SPEED
        ========================================= */
