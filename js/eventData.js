483f54512103ff080c3e082065eb55e455991063
trivias_wrestling <totaltrivialelite@gmail.com>
2026-09-22 04:37:52 -0300

    Update eventData.js

--- js/eventData.js
+++ js/eventData.js
@@ -216,13 +216,19 @@ addPLE("tlc-2024","TLC 2024","22/01/2024","ROAD","images/events/tlc-2024.jpg");
 addPLE("survivor-series-wargames-2023","SURVIVOR SERIES: WARGAMES 2023","17/12/2023","BOOK","images/events/survivor-series-wargames-2023.jpg");
 addPLE("crown-jewel-2023","CROWN JEWEL 2023","27/11/2023","ROAD","images/events/crown-jewel-2023.jpg");
 addPLE("fastlane-2023","FASTLANE 2023","29/10/2023","BOOK","images/events/fastlane-2023.jpg");
-addPLE("payback-2023","PAYBACK 2023","02/10/2023","ROAD","images/events/payback-2023.jpg");
+
+addPLE("payback-2023","PAYBACK 2023","02/10/2023","ROAD","images/events/payback-2023.jpg",[
+{type:"TAG TEAM",position:"OPENER",championship:"WWE World Tag Team Championship",team1:["Bryan Danielson","Jon Moxley"],team2:["Pete Dunne","Sheamus"],score1:3,score2:1},
+{type:"SINGLES",championship:"World Heavyweight Championship",wrestler1:"Cody Rhodes",wrestler2:"GUNTHER",score1:8,score2:1},
+{type:"SINGLES",championship:"Intercontinental Championship",wrestler1:"Chris Jericho",wrestler2:"Dominik Mysterio",score1:5,score2:3},
+{type:"SINGLES",position:"MAIN EVENT",championship:"Undisputed WWE Championship",wrestler1:"Bryan Danielson",wrestler2:"Jeff Jarrett",score1:2,score2:5}
+]);
 
 addPLE("summerslam-2023","SUMMERSLAM 2023","03/09/2023","BOOK","images/events/summerslam-2023.jpg",[
 {type:"SINGLES",position:"OPENER",championship:"NXT Championship",wrestler1:"Jeff Jarrett",wrestler2:"Karrion Kross",score1:6,score2:3},
-{type:"TRIPLE THREAT",championship:"Intercontinental Championship",participants:["Chris Jericho","AJ Styles","Gunther"],scores:[8,1,1]},
+{type:"TRIPLE THREAT",championship:"Intercontinental Championship",participants:["Chris Jericho","AJ Styles","GUNTHER"],scores:[8,1,1]},
 {type:"SINGLES",championship:"United States Championship",wrestler1:"Adam Cole",wrestler2:"Jon Moxley",score1:5,score2:3},
-{type:"SINGLES",championship:"World Heavyweight Championship",wrestler1:"Cody Rhodes",wrestler2:"Ricochet",score1:6,score2:1},
+{type:"SINGLES",championship:"World Heavyweight Championship",wrestler1:"Cody Rhodes",wrestler2:"RICOCHET",score1:6,score2:1},
 {type:"SINGLES",position:"MAIN EVENT",championship:"Undisputed WWE Championship",wrestler1:"Bryan Danielson",wrestler2:"Kevin Owens",score1:6,score2:4}
 ]);
 
