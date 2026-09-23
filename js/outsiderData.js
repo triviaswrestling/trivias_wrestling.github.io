eb7198200cdc28a9da2e58859d7aada219b8995e
trivias_wrestling <totaltrivialelite@gmail.com>
2026-09-22 22:04:01 -0300

    Update outsiderData.js

--- js/outsiderData.js
+++ js/outsiderData.js
@@ -5,6 +5,7 @@
    ========================================= */
 
 const outsiderData = {
+
 // SPEED #2
 "outsider-speed-2":{
     brand:"SPEED",
@@ -18,22 +19,36 @@ const outsiderData = {
         "speed-7":{date:"19/09/2024",matches:[["Rey Mysterio","AJ Styles",1,2]]}
     },
     finalEvent:"bash-in-berlin-2024",
-finalDate:"23/09/2024",
-finalMatches:[["Edge","AJ Styles",5,0]]},
-  
-    /* =========================================
-       SPEED
-       ========================================= */
-    "outsider-speed-1":{
-       name:"SPEED #1",
-        brand:"SPEED",
-        promotion:"WWE",
-        format:"BRACKET",
-        roster:["Bron Breakker","Aleister Black","Tommaso Ciampa","CM Punk","Jey Uso","Edge","Rob Van Dam","Wade Barrett"],
-        shows:{
-            "speed-1":{date:"08/08/2024",matches:[["Bron Breakker","Aleister Black",1,2],["Tommaso Ciampa","CM Punk",2,1]]},
-            "speed-2":{date:"15/08/2024",matches:[["Jey Uso","Edge",1,2],["Rob Van Dam","Wade Barrett",0,3]]},
-            "speed-3":{date:"22/08/2024",matches:[["Aleister Black","Edge",0,3],["Tommaso Ciampa","Wade Barrett",3,0]]}},
-        finalEvent:"summerslam-2024",
-finalDate:"25/08/2024",
-finalMatches:[["Edge","Tommaso Ciampa",3,2]]}};
+    finalDate:"23/09/2024",
+    finalMatches:[
+        {
+            type:"EVENT_REFERENCE",
+            eventId:"bash-in-berlin-2024",
+            matchIndex:0
+        }
+    ]
+},
+
+/* =========================================
+   SPEED
+   ========================================= */
+
+"outsider-speed-1":{
+    name:"SPEED #1",
+    brand:"SPEED",
+    promotion:"WWE",
+    format:"BRACKET",
+    roster:["Bron Breakker","Aleister Black","Tommaso Ciampa","CM Punk","Jey Uso","Edge","Rob Van Dam","Wade Barrett"],
+    shows:{
+        "speed-1":{date:"08/08/2024",matches:[["Bron Breakker","Aleister Black",1,2],["Tommaso Ciampa","CM Punk",2,1]]},
+        "speed-2":{date:"15/08/2024",matches:[["Jey Uso","Edge",1,2],["Rob Van Dam","Wade Barrett",0,3]]},
+        "speed-3":{date:"22/08/2024",matches:[["Aleister Black","Edge",0,3],["Tommaso Ciampa","Wade Barrett",3,0]]}
+    },
+    finalEvent:"summerslam-2024",
+    finalDate:"25/08/2024",
+    finalMatches:[
+        ["Edge","Tommaso Ciampa",3,2]
+    ]
+}
+
+};
